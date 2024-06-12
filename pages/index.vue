a57b484432ba2a6cce97269512a1f18c6bc464e5
Aditya Kadam <adityakadam@Adityas-MacBook-Air-2.local>
2024-06-12 09:45:26 +0100

    package updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <template>
    <Page slug="/" />
+
 </template>
 
 <script setup>
-// can move this to app.vue if needed
-// learn more at https://garchi.co.uk/documentation/1.0/headlessweb/nuxt
 useHead({
     script: [
         {
